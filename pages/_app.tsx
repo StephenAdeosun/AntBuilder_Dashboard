fd452f0894156749d792a35ba996fc06f5d58804
StephenAdeosun <stephenadeosun411@gmail.com>
2023-10-12 13:38:40 +0100

    feat:admin dashboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app'; // Import the AppProps type
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
 export default MyApp;
